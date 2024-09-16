2442118afa733f432e148743c5e4e324e00fb1aa
Daniel Romero <dandres508@gmail.com>
2024-09-16 16:52:05 -0500

    Addes sinopsis

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Película: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0 )/ 3;
+        System.out.println(media);
+
+        String sinopsis = """
+                Matrix es una paradoja
+                la mejor película del fin del milenio
+                fue lanzada en:
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
     }
 }
