1ed0ce681dd76c44603e9a753a155c87c74c8ebb
Daniel Romero <dandres508@gmail.com>
2024-09-17 10:13:27 -0500

    added Decisiones

---
+++ src/Decisiones.java
@@ -0,0 +1,15 @@
+public class Decisiones {
+
+    public static void main(String[] args) {
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        if (fechaDeLanzamiento >= 2022){
+            System.out.println("Películas más populares");
+        } else {
+            System.out.println("Películas retro que aún vale la pena ver");
+        }
+    }
+}
