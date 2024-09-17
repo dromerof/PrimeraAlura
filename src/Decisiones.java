ebf558eba492e8b814735d7c0030841b6f57c1ed
Daniel Romero <dandres508@gmail.com>
2024-09-17 10:24:56 -0500

    added condicionales en decisiones

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -5,11 +5,19 @@ public class Decisiones {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
         double notaDeLaPelicula = 8.2;
+        String tipoPlan = "plus";
 
         if (fechaDeLanzamiento >= 2022){
             System.out.println("Películas más populares");
         } else {
             System.out.println("Películas retro que aún vale la pena ver");
+        }
+
+
+        if (incluidoEnElPlan && tipoPlan.equals("plus") ){
+            System.out.println("Disfrute de su película");
+        } else {
+            System.out.println("La película no esta incluida en su plan actual");
         }
     }
 }
